e8542be41db5197cec515320524ceb4cd9807bc0
nvhung <hungthatsu@gmail.com>
2024-01-01 16:28:23 +0700

    first commit this code won't work, will be fix in the latter version

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -4,36 +4,39 @@ return {
     lazy = false,
     config = function()
       require("mason").setup()
-    end,
+    end
+  },
+  {
+    "williamboman/nvim-lsp-installer",
+    config = function()
+      require("nvim-lsp-installer").setup({
+      automatic_installation = true, 
+      ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+      }
+    })
+    end
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "tsserver", "csharp_ls","ccls"}
+      })
+    end
   },
   {
     "neovim/nvim-lspconfig",
-    lazy = false,
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilites = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilites = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilites = capabilities,
-      })
-
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+    end
+  }
 }
